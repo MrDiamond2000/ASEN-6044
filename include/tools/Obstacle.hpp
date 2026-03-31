701e1fc422196bfc22f28cbcb1fc4b8edd5e390a
Kong Yu Kang <yukang.kong.uni@gmail.com>
2026-03-31 17:03:18 -0600

    add path planning with some basic debug

---
+++ include/tools/Obstacle.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <vector>
+#include <Eigen/Core>
+
+// #include "tools/Serializer.h"
+
+namespace path_planning {
+
+class Polygon {
+    public:
+        // /// @brief Used for deserialization
+        // Polygon() = default;
+        
+        // /// @brief Construct from a set of vertices ASSUMED to be in counter clock wise order
+        // /// @param vertices_ccw Ordered counter clock wise order
+        // Polygon(std::vector<Eigen::Vector2d>& vertices_ccw);
+        
+        /// @brief Access the vertices in memory of the obstacle (counter clock wise coordinates)
+        /// @return Reference to stored CCW vertices
+        std::vector<Eigen::Vector2d>& verticesCCW() {
+            return m_vertices_ccw;
+        } 
+
+        /// @brief Access the vertices in memory of the obstacle (counter clock wise coordinates)
+        /// @return Reference to stored CCW vertices
+        const std::vector<Eigen::Vector2d>& verticesCCW() const{
+            return m_vertices_ccw;
+        }; 
+
+    private:
+        std::vector<Eigen::Vector2d> m_vertices_ccw;
+};
+
+using Obstacle2D = Polygon;
+
+}
---
+++ src/tools/HelpfulClass.cpp
@@ -0,0 +1,104 @@
+#include "tools/HelpfulClass.hpp"
+
+Point2DCollisionChecker::Point2DCollisionChecker(const path_planning::Environment2D& enviroument_)
+: env(enviroument_){
+    std::vector<std::pair<double,double>> bounds;
+    bounds.push_back({enviroument_.x_min, enviroument_.x_max});
+    bounds.push_back({enviroument_.y_min, enviroument_.y_max});
+    setBounds(bounds);
+}
+
+bool Point2DCollisionChecker::isCollide(const Eigen::VectorXd& point) {
+    for (size_t ob_idx = 0; ob_idx < env.obstacles.size(); ++ob_idx) {
+        if (thisObstacle(ob_idx, point)){
+            return true; // Point is inside this polygons
+        }
+    }
+    return false; // Point is outside all polygons
+    // Implementation
+}
+
+bool Point2DCollisionChecker::isCollide2P(const Eigen::VectorXd& point1_, const Eigen::VectorXd& point2_){
+    Eigen::VectorXd one_2_two = point2_ - point1_;
+    double distance = one_2_two.norm();
+    int sec_num = 1;
+    while(distance/sec_num > GAP){
+        for(int i = 0; i < sec_num; i++){
+            Eigen::VectorXd check_location = point1_ + one_2_two*(1+2*i)/(sec_num*2);
+            if (isCollide(check_location))
+            {
+                return true;
+            }
+        }
+        sec_num = sec_num * 2;
+    }
+    return false;
+}
+
+bool Point2DCollisionChecker::thisObstacle(int ob_idx_, const Eigen::VectorXd& point_){
+    const path_planning::Obstacle2D& obstacle = env.obstacles[ob_idx_];
+    double cross_product;
+
+    int n = obstacle.verticesCCW().size();
+    for (int vertix_idx = 0; vertix_idx < n; ++vertix_idx) {
+        const Eigen::VectorXd& v1 = obstacle.verticesCCW()[vertix_idx];
+        const Eigen::VectorXd& v2 = obstacle.verticesCCW()[(vertix_idx + 1) % n];
+        Eigen::VectorXd edge = v1 - v2;
+        Eigen::VectorXd point_to_v1 = point_ - v1;
+        cross_product = edge(0) * point_to_v1(1) - edge(1) * point_to_v1(0);
+        if (cross_product > 0){
+            return false; // Point is outside of this obstacle
+        }
+    }
+    return true; // Point is inside this obstacle
+}
+
+Point2DCollisionCheckerGrid::Point2DCollisionCheckerGrid(const path_planning::GridCSpace2D_T<int8_t>& map_)
+: map(map_){
+    std::vector<std::pair<double,double>> bounds;
+    bounds.push_back({map.x0Bounds().first, map.x0Bounds().second});
+    bounds.push_back({map.x1Bounds().first, map.x1Bounds().second});
+    setBounds(bounds);
+}
+
+bool Point2DCollisionCheckerGrid::isCollide(const Eigen::VectorXd& point_){
+    return(map.inCollision(point_(0), point_(1)));
+}
+
+bool Point2DCollisionCheckerGrid::isCollide2P(const Eigen::VectorXd& point1_, const Eigen::VectorXd& point2_){
+    Eigen::VectorXd one_2_two = point2_ - point1_;
+    double distance = one_2_two.norm();
+    int sec_num = 1;
+    while(distance/sec_num > GAP){
+        for(int i = 0; i < sec_num; i++){
+            Eigen::VectorXd check_location = point1_ + one_2_two*(1+2*i)/(sec_num*2);
+            if (isCollide(check_location))
+            {
+                return true;
+            }
+        }
+        sec_num = sec_num * 2;
+    }
+    return false;
+}
+
+bool MultiAgentPoint2DCollisionCheckerGrid::isCollide(int agent_idx_, const Eigen::VectorXd& point_){
+    return(maps[agent_idx_].inCollision(point_(0), point_(1)));
+}
+
+bool MultiAgentPoint2DCollisionCheckerGrid::isCollide2P(int agent_idx_, const Eigen::VectorXd& point1_, const Eigen::VectorXd& point2_){
+    Eigen::VectorXd one_2_two = point2_ - point1_;
+    double distance = one_2_two.norm();
+    int sec_num = 1;
+    while(distance/sec_num > GAP){
+        for(int i = 0; i < sec_num; i++){
+            Eigen::VectorXd check_location = point1_ + one_2_two*(1+2*i)/(sec_num*2);
+            if (isCollide(agent_idx_, check_location))
+            {
+                return true;
+            }
+        }
+        sec_num = sec_num * 2;
+    }
+    return false;
+}
