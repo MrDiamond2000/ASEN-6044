5e8fc53db4c039304b657f945c45358029003d78
Kong Yu Kang <yukang.kong.uni@gmail.com>
2026-03-31 18:05:13 -0600

    add method to create obstacles

--- include/tools/Obstacle.hpp
+++ include/tools/Obstacle.hpp
@@ -12,9 +12,11 @@ class Polygon {
         // /// @brief Used for deserialization
         // Polygon() = default;
         
-        // /// @brief Construct from a set of vertices ASSUMED to be in counter clock wise order
-        // /// @param vertices_ccw Ordered counter clock wise order
-        // Polygon(std::vector<Eigen::Vector2d>& vertices_ccw);
+        /// @brief Construct from a set of vertices ASSUMED to be in counter clock wise order
+        /// @param vertices_ccw Ordered counter clock wise order
+        Polygon(std::vector<Eigen::Vector2d>& vertices_ccw){
+            m_vertices_ccw = vertices_ccw;
+        }
         
         /// @brief Access the vertices in memory of the obstacle (counter clock wise coordinates)
         /// @return Reference to stored CCW vertices
