37f4f9985c2c73cb0c24c94ae5cf3982eb5863ff
MrDiamond2000 <qllevins@icloud.com>
2026-04-06 00:50:18 -0600

    Working particle filter code

---
+++ src/PF/plotResults.py
@@ -0,0 +1,154 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.patches import Polygon
+import yaml
+
+# Define parameters from yaml file
+with open("src/PF/ParticleFilterParams.yaml", "r") as file:
+    config = yaml.safe_load(file)
+
+maxX = config["environment"]["maxX"] 
+maxY = config["environment"]["maxY"]
+fov = np.pi/config["sensor"]["fovFractionOfPi"]
+sensorRange = config["sensor"]["range"]
+gridSizeX = config["particleFilter"]["gridSizeX"]
+gridSizeY = config["particleFilter"]["gridSizeY"]
+observedDecayRate = config["particleFilter"]["observedDecayRate"]
+maxObservedScore = config["particleFilter"]["maxObservedScore"]
+
+# Define obstacles - make sure these are the same as the ones in main.cpp!
+obstacles = [np.array([[2,3],[8,3],[8,4],[2,4]]), np.array([[2,6],[8,6],[8,7],[2,7]])]
+
+# Load the most recent output csv file
+data = np.loadtxt(config["misc"]["outputFile"], delimiter=",")
+
+# Figure setup
+fig, ax = plt.subplots(figsize=(7,7))
+ax.set_xlim(0, maxX)
+ax.set_ylim(0, maxY)
+ax.set_aspect("equal")
+ax.set_xlabel("X Position")
+ax.set_ylabel("Y Position")
+
+# Create a heatmap for visualizing the observed grid
+observedGrid = np.zeros((gridSizeX, gridSizeY))
+heatMap = ax.imshow(observedGrid.T, extent=[0, maxX, 0, maxY], origin="lower", cmap="coolwarm", vmin=0, vmax=maxObservedScore, alpha=0.5, interpolation="nearest")
+
+# Create a colorbar
+colorBar = plt.colorbar(heatMap, ax=ax)
+colorBar.set_label("Observed Score")
+
+# Create obstacles in figure
+for obstacle in obstacles:
+    shape = Polygon(obstacle, closed=True, facecolor="gray", edgecolor="black", alpha=1)
+    ax.add_patch(shape)
+
+# Plot the particles, vehicle, target, and filter estimate over time
+particlesPlot, = ax.plot([], [], ".", color="blue", markersize=2, label="Particles")
+vehiclePlot, = ax.plot([], [], "o", color="lime", markersize=10, label="Vehicle")
+targetPlot, = ax.plot([], [], "x", color="red", markersize=10, label="Target")
+estimatePlot, = ax.plot([], [], "*", color="lime", markersize=10, label="Estimate")
+
+# Plot the field of view path over time
+fovPatch = Polygon([[0,0]], closed=True, color="lime", alpha=0.2, label="Sensor FOV")
+ax.add_patch(fovPatch)
+
+# Add a legend to the plot
+ax.legend(loc="lower center", bbox_to_anchor=(0.5, 1.05), ncol=3)
+plt.tight_layout()
+
+# Checks if points are in counter clockwise order
+def counterClockwise(p1, p2, p3):
+    return (p3[1] - p1[1])*(p2[0] - p1[0]) > (p2[1] - p1[1])*(p3[0] - p1[0])
+
+# Checks if the two segments intersect
+def segmentsIntersect(p1, p2, p3, p4):
+    return (counterClockwise(p1, p3, p4) != counterClockwise(p2, p3, p4)) and (counterClockwise(p1, p2, p3) != counterClockwise(p1, p2, p4))
+
+# Check whether the vehicle can observe the provided grid cell location (if there is an obstacle in the way)
+def lineOfSight(vehicle, target):
+    for obstacle in obstacles:
+        for i in range(len(obstacle)):
+            obstacleVertex1 = obstacle[i]
+            obstacleVertex2 = obstacle[(i+1)%len(obstacle)]
+            if segmentsIntersect(vehicle, target, obstacleVertex1, obstacleVertex2):
+                return False
+    return True
+
+# Calculate the fov shape of the sensor
+def fovShape(vehicle, heading):
+    headingAngle = np.arctan2(heading[1], heading[0])
+    fovVertexAngles = np.linspace(headingAngle - fov/2, headingAngle + fov/2, 50)
+
+    fovShapeVertices = [vehicle]
+    for angle in fovVertexAngles:
+        fovShapeVertices.append([vehicle[0] + sensorRange*np.cos(angle), vehicle[1] + sensorRange*np.sin(angle)])
+
+    return np.array(fovShapeVertices)
+
+# Check if a grid cell is observed
+def observed(cellPosition, vehicle, heading):
+    dx = cellPosition[0] - vehicle[0]
+    dy = cellPosition[1] - vehicle[1]
+
+    distanceSquared = dx*dx + dy*dy
+    if distanceSquared > sensorRange**2: return False
+
+    dot = dx*heading[0] + dy*heading[1]
+
+    if dot <= np.sqrt(distanceSquared)*np.cos(fov/2.0):
+        return False
+
+    if not lineOfSight(vehicle, cellPosition):
+        return False
+
+    return True
+
+# Update the frame of the animation
+def updateFrame(frameIndex):
+    global observedGrid
+
+    # Get the positions of all objects of interest from the current row of the csv file
+    currentPositions = data[frameIndex]
+    vehicle = currentPositions[0:2]
+    heading = currentPositions[2:4]
+    estimate = currentPositions[4:6]
+    target = currentPositions[6:8]
+    particles = currentPositions[8:].reshape(-1,2)
+
+    # Update the plots with the current vehicle, estimate, target, and particle positions
+    vehiclePlot.set_data([vehicle[0]], [vehicle[1]])
+    targetPlot.set_data([target[0]], [target[1]])
+    estimatePlot.set_data([estimate[0]], [estimate[1]])
+    particlesPlot.set_data(particles[:,0], particles[:,1])
+
+    # Update the sensor fov shape
+    fovPatch.set_xy(fovShape(vehicle, heading))
+
+    # Update the observed grid by decaying the values and then increase the score of the currently observed grid cells
+    observedGrid *= observedDecayRate
+
+    for i in range(gridSizeX):
+        for j in range(gridSizeY):
+            gridCell = np.array([(i + 0.5)*maxX/gridSizeX, (j + 0.5)*maxY/gridSizeY])
+
+            if observed(gridCell, vehicle, heading):
+                observedGrid[i, j] = min(observedGrid[i, j] + 1.0, maxObservedScore)
+
+    # Redefine the heatmap based on the current observed grid
+    heatMap.set_array(observedGrid.T)
+
+    # Dynamically update the plot title
+    ax.set_title(f"Particle Filter Estimate at Frame {frameIndex}")
+
+    return (particlesPlot, vehiclePlot, targetPlot, estimatePlot, fovPatch, heatMap)
+
+# Create animation
+animation = FuncAnimation(fig, updateFrame, frames=data.shape[0], interval=20, repeat=False)
+
+# Display the animation
+plt.show()
+
+# Save animation video as a file
+# animation.save("particle_filter.mp4", fps=20)
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include "core/MySamplingBasedPlanners.hpp"
+#include "PF/ParticleFilter.hpp"
+#include <fstream>
+#include <yaml-cpp/yaml.h>
 
 int main(){
     // build environment
@@ -14,18 +17,149 @@ int main(){
 
     // test collision checker
     Point2DCollisionChecker collision_checker(env);
-    LOG("Is (3, 3.5) in collsion: " << collision_checker.isCollide(Eigen::Vector2d(3, 3.5)));
+    LOG("Is (3, 3.5) in collision: " << collision_checker.isCollide(Eigen::Vector2d(3, 3.5)));
 
+    // load in parameters from the parameter yaml file
+    YAML::Node config = YAML::LoadFile("src/PF/ParticleFilterParams.yaml");
+
+    double maxX = config["environment"]["maxX"].as<double>();
+    double maxY = config["environment"]["maxY"].as<double>();
+
+    double rrtBias = config["pathPlanner"]["bias"].as<double>();
+    double rrtIteration = config["pathPlanner"]["iteration"].as<double>();
+    double rrtStepSize = config["pathPlanner"]["stepSize"].as<double>();
+
+    int Ns = config["particleFilter"]["Ns"].as<int>();
+    double stepSize = config["particleFilter"]["stepSize"].as<double>();
+    double resampleThreshold = config["particleFilter"]["resampleThreshold"].as<double>();
+    int gridSizeX = config["particleFilter"]["gridSizeX"].as<int>();
+    int gridSizeY = config["particleFilter"]["gridSizeY"].as<int>();
+    int maxPropogationAttempts = config["particleFilter"]["maxPropogationAttempts"].as<int>();
+    double maxObservedScore = config["particleFilter"]["maxObservedScore"].as<double>();
+    double observedDecayRate = config["particleFilter"]["observedDecayRate"].as<double>();
+
+    double vehicleInitialX = config["vehicle"]["vehicleInitialX"].as<double>();
+    double vehicleInitialY = config["vehicle"]["vehicleInitialY"].as<double>();
+    double vehicleInitialHeading = config["vehicle"]["vehicleInitialHeading"].as<double>();
+
+    double fovFractionOfPi = config["sensor"]["fovFractionOfPi"].as<double>();
+    double range = config["sensor"]["range"].as<double>();
+
+    int maxIterations = config["misc"]["maxIterations"].as<int>();
+    std::string outputFile = config["misc"]["outputFile"].as<std::string>();
 
     // test path planner
-    MyRRT rrt(0.05, 1000, 0.5);
+    MyRRT rrt(rrtBias, rrtIteration, rrtStepSize);
     rrt.set_environment(env);
-    path_planning::Path2D path_output = rrt.plan(Eigen::Vector2d(1,1), Eigen::Vector2d(5,5));
+    //path_planning::Path2D path_output = rrt.plan(Eigen::Vector2d(1,1), Eigen::Vector2d(5,5));
 
-    LOG("Path valid: " << path_output.valid);
-    if (path_output.valid){
-        for (size_t i = 0; i < path_output.waypoints.size(); i++){
-            LOG(path_output.waypoints[i].transpose());
+    //LOG("Path valid: " << path_output.valid);
+    //if (path_output.valid){
+    //    for (size_t i = 0; i < path_output.waypoints.size(); i++){
+    //        LOG(path_output.waypoints[i].transpose());
+    //    }
+    //}
+
+    // test particle filter
+    BaseCollisionChecker<Eigen::VectorXd>& lineOfSightChecker = collision_checker;
+
+    // create particle filter object and pass in the number of particles and environment size
+    ParticleFilter pf(Ns, maxX, maxY, gridSizeX, gridSizeY);
+
+    // generate initial particles, target, and estimate
+    pf.setMaxPropogationAttempts(maxPropogationAttempts);
+    pf.setMaxObservedScore(maxObservedScore);
+    pf.setObservedDecayRate(observedDecayRate);
+    Eigen::Vector2d target = pf.initializeParticles(collision_checker);
+    Eigen::Vector2d estimate = pf.estimate();
+    Eigen::Vector2d direction;
+    Eigen::Vector2d headingVector;
+
+    // open output csv file
+    std::ofstream file(outputFile, std::ios::out);
+
+    // initialize a search vehicle
+    Eigen::Vector2d vehicle(vehicleInitialX, vehicleInitialY);
+
+    // define sensor parameters
+    double fov = M_PI/fovFractionOfPi;
+    double fovCosine = cos(fov/2.0);
+
+    // run particle filter iterations and save data to a csv
+    bool found = false;
+    double currentHeading = vehicleInitialHeading;
+    for (int i = 0; i < maxIterations; i++) {
+
+        // calculate the vehicle heading
+        direction = estimate - vehicle;
+
+        if (direction.norm() > 1e-6) {
+            direction.normalize();
+            currentHeading = atan2(direction(1), direction(0));
         }
+
+        headingVector = {cos(currentHeading), sin(currentHeading)};
+
+        // output the vehicle, estimate, and target positions to the csv file
+        file << vehicle(0) << "," << vehicle(1) << "," << headingVector(0) << ","  << headingVector(1) << "," << estimate(0) << "," << estimate(1) << "," << target(0) << "," << target(1);
+        
+        // output the particle positions to the csv file
+        for (auto& p : pf.particles) {
+            file << "," << p.position(0) << "," << p.position(1);
+        }
+        file << "\n";
+
+        // run the particle filter
+        pf.step(collision_checker, lineOfSightChecker, vehicle, headingVector, fovCosine, range, stepSize, resampleThreshold);
+        estimate = pf.estimate();
+
+        // check if the target has been detected
+        if (pf.detectTarget(target, vehicle, headingVector, lineOfSightChecker, fovCosine, range)) {
+            std::cout << "Target FOUND at iteration " << i << std::endl;
+            std::cout << "True target:      " << target.transpose() << std::endl;
+            std::cout << "Estimate:         " << pf.estimate().transpose() << std::endl;
+            std::cout << "Vehicle Position: " << vehicle.transpose() << std::endl;
+            found = true;
+            break;
+        }
+
+        // plan a path towards the filter estimate
+        path_planning::Path2D path = rrt.plan(vehicle, estimate);
+
+        if (path.valid && path.waypoints.size() > 1) {
+            // move one step along the path, but normalize to the step size (velocity) of the vehicle to ensure consistency
+            Eigen::Vector2d next = path.waypoints[1];
+            Eigen::Vector2d delta = next - vehicle;
+
+            if (delta.norm() > stepSize) {
+                delta.normalize();
+                delta *= stepSize;
+            }
+
+            vehicle += delta;
+        }
+
+        // propogate the target position for the next iteration
+        pf.propagate(target, collision_checker, stepSize);
     }
+
+    // output the final particle filter estimate to the csv
+    file << vehicle(0) << "," << vehicle(1) << "," << headingVector(0) << ","  << headingVector(1) << "," << estimate(0) << "," << estimate(1) << "," << target(0) << "," << target(1);
+
+    // output the final particle positions to the csv
+    for (auto& p : pf.particles) {
+            file << "," << p.position(0) << "," << p.position(1);
+        }
+        file << "\n";
+
+    // close the output csv file
+    file.close();
+
+    // if the filter did not locate the target, print it's final estimate
+    if (!found) {
+        std::cout << "Target NOT found.\n";
+        std::cout << "Final estimate: " << estimate.transpose() << std::endl;
+    }
+
+    return 0;
 }
